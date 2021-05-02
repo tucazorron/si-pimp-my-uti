a4e37ff8845297294c022687d2ca8896d5fe8059
artur zorron <tucazorron@gmail.com>
2021-05-02 12:03:54 -0300

    create patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,17 +1,37 @@
 class PatientsController < ApplicationController
+
   def index
   end
 
   def create
-    @patient = Patient.new
-    
+    @patient = Patient.new(patient_params)
+
+    if @patient.save
+      render json: @patient, status: :created, location: @patient
+
+    else
+      render json: @patient.errors, status: :unprocessable_entity
+
+    end
   end
 
   def destroy
+    @patient.destroy
   end
 
   def show
     @patient = Patient.find(params[:id])
     render json: @patient
   end
+
+  private
+
+  def set_patient
+    @patient = Patient.find(params[:id])
+  end
+
+  def patient_params
+    params.require(:patient).permit(:name, :rg, :age, :cause, :description, :gender_id)
+  end
+
 end
