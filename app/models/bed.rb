f5c39e47c65e3ac19721ac078ec32887977a5746
Vinicius Costa <viniciuscosta0197@gmail.com>
2021-05-09 10:24:07 -0300

    add before save to bed

--- app/models/bed.rb
+++ app/models/bed.rb
@@ -1,5 +1,12 @@
 class Bed < ApplicationRecord
+  before_save :default_values
+
   has_one :bed_type
   has_one :wing
   belongs_to :patient, optional: true
+
+
+  def default_values
+    self.busy ||= false if self.busy.nil?
+  end
 end
